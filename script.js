cdc7cce4fe2b1b1cdb4f8540359513aa8229ab5e
ABC <abc@gmail.com>
2023-08-20 10:07:27 +0530

    First message

---
+++ script.js
@@ -0,0 +1,19 @@
+const countValue = document.querySelector('#counter');
+
+function increment(){
+    //get the value from UI
+    let value = parseInt(countValue.innerText);
+    //update the value
+    value = value + 1;
+    //set the value onto UI
+    countValue.innerText = value;
+};
+
+function decrement(){
+    //get the value from UI
+    let value = parseInt(countValue.innerText);
+    //update the value
+    value = value - 1;
+    //set the value onto UI
+    countValue.innerText = value;
+};
